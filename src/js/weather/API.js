3a59f63003e2f6caa9937c63992489d040cc2026
Porkenvolf <89871231+porkenvolf@users.noreply.github.com>
2023-08-18 23:21:18 -0300

    Momentarily disabled ip location
    Mixed content error is blocking access to the location api

--- src/js/weather/API.js
+++ src/js/weather/API.js
@@ -7,10 +7,12 @@ async function queryLocationAPI() {
   return location;
 }
 export default async function queryWeatherAPI(_location) {
-  const location = _location || (await queryLocationAPI());
+  const location = _location || 'montevideo';
   const requestWeather =
     await fetch(`https://api.weatherapi.com/v1/forecast.json?key=6b50d5938f0b4173a06202148231208&q=${location}&days=10&aqi=no&alerts=no
     `);
   const data = await requestWeather.json();
   return data;
 }
+
+// TODO mixed content, remove ip location?
